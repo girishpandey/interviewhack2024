ae96aa88b4503fd9d4bcbbb4e45d39ea36393867
girishpandey <umeaurjava@gmail.com>
2024-08-18 22:58:06 +0530

    java interview prep 2024

--- src/com/gcp/java8/stream/StreamTest.java
+++ src/com/gcp/java8/stream/StreamTest.java
@@ -25,4 +25,33 @@ public class StreamTest {
 		Integer number = String.valueOf(num).chars().map(n->Character.getNumericValue(n)).sum();
 		System.out.println(number);
 	}
+	public void reduceExample(){
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+		int sum = numbers.stream().reduce(0, Integer::sum);
+		System.out.println(sum);
+		
+		List<Integer> numbers2 = Arrays.asList(3, 5, 9, 1);
+		int max = numbers2.stream().reduce(Integer.MIN_VALUE, Integer::max);
+		System.out.println(max);
+	}
+	public void flatmapexample(){
+		List<List<String>> listOfLists = Arrays.asList(
+			    Arrays.asList("a", "b", "c"),
+			    Arrays.asList("d", "e"),
+			    Arrays.asList("f", "g", "h")
+			);
+
+			List<String> flatList = listOfLists.stream()
+			                                   .flatMap(List::stream)
+			                                   .collect(Collectors.toList());
+			System.out.println(flatList); // Output: [a, b, c, d, e, f, g, h]
+
+			List<String> words = Arrays.asList("hello", "world");
+
+			List<Character> characters = words.stream()
+			                                  .flatMap(word -> word.chars().mapToObj(c -> (char) c))
+			                                  .collect(Collectors.toList());
+
+			System.out.println(characters); // Output: [h, e, l, l, o, w, o, r, l, d]
+	}
 }
